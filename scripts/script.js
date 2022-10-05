cae91d8271af86b51693e44f09ae1fc6d96e43f9
Britjeee <112875602+Britjeee@users.noreply.github.com>
2022-10-05 23:06:34 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 // JavaScript Document
+
+// javascript voor hamburger menu toggle
 var hamburgerMenu = document.querySelector("header nav > ul:nth-of-type(1) > li:nth-of-type(3) > button");
 
 hamburgerMenu.addEventListener("click", menuToggle);
@@ -8,7 +10,35 @@ function menuToggle() {
     menuInhoud.classList.toggle("open");
 }
 
-console.log("hi");
+// javascript voor feedback nieuwsbrief aanmelden
+var aanmeldButton = document.querySelector("main > section:nth-of-type(4) button")
+
+aanmeldButton.addEventListener("click", aanmeldenFeedback);
+
+function aanmeldenFeedback() {
+    var inputWaarde = document.querySelector("main > section:nth-of-type(4) input").value.indexOf("@");
+    aanmeldenGelukt = "true"
+
+    if (inputWaarde == -1) {
+        alert("De e-mail die u heeft gebruikt is ongeldig. Probeer opnieuw!");
+        aanmeldenGelukt = "false";
+      }
+    
+      if (aanmeldenGelukt == "false") {
+        alert("Vul een geldig e-mailadres in.");
+        return false;
+      }
+      
+      if (aanmeldenGelukt = "true") {
+        alert("Gelukt! U ontvangt voortaan automatisch updates per mail van Moet Ik Naar De Dokter?");
+      }
+}
+
+
+
+
+
+console.log(aanmeldButton);
 
 
 
