2c6b7011432d9979724d66493b69940603da1bbe
Britjeee <112875602+Britjeee@users.noreply.github.com>
2022-10-05 12:09:20 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
+var hamburgerMenu = document.querySelector("header nav > ul:nth-of-type(1) > li:nth-of-type(3) > button");
+
+hamburgerMenu.addEventListener("click", menuToggle);
+
+function menuToggle() {
+    var menuInhoud = document.querySelector("header nav > ul:nth-of-type(2)");
+    menuInhoud.classList.toggle("open");
+}
+
 console.log("hi");
+
+
+
+
+// var menuButton = document.querySelector("header button");
+
+// menuButton.addEventListener("click", menuToggle);
+
+// function menuToggle() {
+// 	var deNav = document.querySelector("header > nav");
+	
+// 	deNav.classList.toggle("open");
+// }
